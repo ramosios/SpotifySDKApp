936ea73e32276846d95312773e397fa4be9ac266
jorgeramos2 <jorge.ramos1996@gmail.com>
2024-12-17 13:26:47 -0600

    Initial commit

---
+++ SpotifySDKApp/SpotifySDKApp/SpotifySDKAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifySDKAppApp.swift
+//  SpotifySDKApp
+//
+//  Created by Jorge Ramos on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotifySDKAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
