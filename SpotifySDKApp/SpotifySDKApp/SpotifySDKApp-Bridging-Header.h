670c45b85ca99d6c3d81fab43ccd9a71408d2692
jorgeramos2 <jorgeramos.1996@outlook.com>
2024-12-18 12:24:37 -0600

    header file objective c
    Bridging Header

---
+++ SpotifySDKApp/SpotifySDKApp/SpotifySDKApp-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  SpotifySDKApp-Bridging-Header.h
+//  SpotifySDKApp
+//
+//  Created by Jorge Ramos on 18/12/24.
+//
+
+#ifndef SpotifySDKApp_Bridging_Header_h
+#define SpotifySDKApp_Bridging_Header_h
+
+#import <SpotifyiOS/SpotifyiOS.h>
+#endif /* SpotifySDKApp_Bridging_Header_h */
